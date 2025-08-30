f29882211eb5bb2cecad3eeb2db451797ad83594
Uma Shree <umashree31jan@gmail.com>
2025-08-30 18:29:08 +0530

    added required files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="First_Nurse",
+    version="0.1.0",
+    author="Uma Shree",
+    author_email="umashree31jan@gmail.com",
+    packages=find_packages(),
+    intall_requires=[]
+
+
+
+)
